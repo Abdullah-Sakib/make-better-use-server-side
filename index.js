a9ae4799d9679b4efa2c14fdb0201a87f4421191
Abdullah-Sakib <abdullahsakib888@gmail.com>
2022-11-24 16:24:56 +0600

    Connected the server with the database

--- index.js
+++ index.js
@@ -2,9 +2,18 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.3booq2e.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
 
 app.get('/', (req, res) => {
   res.send('server is running')
