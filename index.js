bc082995f0a5c668a95186e50552e0e40919e4f3
Abdullah-Sakib <abdullahsakib888@gmail.com>
2022-11-25 20:39:48 +0600

    Create payment intent

--- index.js
+++ index.js
@@ -1,109 +1,138 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 const port = process.env.PORT || 5000;
-const jwt = require('jsonwebtoken');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config();
+const jwt = require("jsonwebtoken");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
 
 app.use(cors());
 app.use(express.json());
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.3booq2e.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
-const verifyJWT = (req, res, next) =>{
+const verifyJWT = (req, res, next) => {
   const authHeader = req.headers.authorization;
-  if(!authHeader){
-    return res.status(401).send({message: 'unauthorized access'});
+  if (!authHeader) {
+    return res.status(401).send({ message: "unauthorized access" });
   }
-  const token = authHeader.split(' ')[1];
+  const token = authHeader.split(" ")[1];
   jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-    if(err){
-      return res.status(401).send({message: 'forbidden access'})
+    if (err) {
+      return res.status(401).send({ message: "forbidden access" });
     }
     req.decoded = decoded;
     next();
-  }) 
-}
+  });
+};
 
-async function run(){
-  try{
-    const usersCollection = client.db('resellDB').collection('users');
-    const categoriesCollection = client.db('resellDB').collection('categories');
-    const productsCollection = client.db('resellDB').collection('products');
-    const bookedProductsCollection = client.db('resellDB').collection('bookedProducts');
+async function run() {
+  try {
+    const usersCollection = client.db("resellDB").collection("users");
+    const categoriesCollection = client.db("resellDB").collection("categories");
+    const productsCollection = client.db("resellDB").collection("products");
+    const bookedProductsCollection = client
+      .db("resellDB")
+      .collection("bookedProducts");
 
     //send jwt token
-    app.post('/jwt', async(req, res) => {
+    app.post("/jwt", async (req, res) => {
       const user = req.body;
       const token = jwt.sign(user, process.env.JWT_SECRET);
-      res.send({accessToken: token});
-    })
+      res.send({ accessToken: token });
+    });
 
     //save user data
-    app.post('/users' , async(req,res) => {
+    app.post("/users", async (req, res) => {
       const user = req.body;
-      const query = {email: user.email};
+      const query = { email: user.email };
       const checkUser = await usersCollection.findOne(query);
-      if(checkUser){
-        return res.send({message: 'User already exist'});
+      if (checkUser) {
+        return res.send({ message: "User already exist" });
       }
       const reslut = await usersCollection.insertOne(user);
       res.send(reslut);
     });
 
     // get all categories
-    app.get('/categories', async(req, res) => {
+    app.get("/categories", async (req, res) => {
       const query = {};
       const result = await categoriesCollection.find(query).toArray();
       res.send(result);
-    })
+    });
 
     //save product in database
-    app.post('/products', async(req, res) => {
+    app.post("/products", async (req, res) => {
       const product = req.body;
       const result = await productsCollection.insertOne(product);
       res.send(result);
-    })
+    });
 
     //get category products
-    app.get('/products', async(req, res) => {
+    app.get("/products", async (req, res) => {
       const categoryName = req.query.name;
-      const query = {category: categoryName};
+      const query = { category: categoryName };
       const result = await productsCollection.find(query).toArray();
       res.send(result);
-    })
+    });
 
     //save booked products
-    app.post('/bookedProducts', async(req, res) => {
+    app.post("/bookedProducts", async (req, res) => {
       const bookedProduct = req.body;
       const result = await bookedProductsCollection.insertOne(bookedProduct);
       res.send(result);
-    })
+    });
 
     //get my orders
-    app.get('/bookedProducts', verifyJWT, async(req, res) => {
+    app.get("/bookedProducts", verifyJWT, async (req, res) => {
       const email = req.decoded.email;
-      const query = {userEmail: email};
+      const query = { userEmail: email };
       const result = await bookedProductsCollection.find(query).toArray();
-      res.send(result)
-    })
+      res.send(result);
+    });
+
+    //get single product data
+    app.get("/bookedProducts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await bookedProductsCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.post("/create-payment-intent", async (req, res) => {
+      const price = parseInt(req.body.price) * 100;
+    
+      // Create a PaymentIntent with the order amount and currency
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: price,
+        currency: "usd",
+        "payment_method_types": [
+          "card"
+        ],
+      });
+    
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
+    });
 
 
-  }
-  finally{
-
+  } finally {
   }
 }
-run().catch(error => console.log(error))
+run().catch((error) => console.log(error));
 
-
-app.get('/', (req, res) => {
-  res.send('server is running')
+app.get("/", (req, res) => {
+  res.send("server is running");
 });
 
 app.listen(port, () => {
   console.log(`server is running on port ${port}`);
-})
+});
